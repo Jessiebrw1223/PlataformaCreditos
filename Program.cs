bef760d4c8ee92797c54eb36b555b21d01394e0b
Jessiebrw1223 <tu-email@example.com>
2026-04-25 12:01:19 -0500

    Fix Redis build error for Render

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
 using PlataformaCreditos.Data;
-using StackExchange.Redis;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
